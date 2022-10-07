e4259775add6912f0accd6370956193bdfefb24b
Fitzdaini Dinham <kemonte456@gmail.com>
2022-10-07 13:05:03 -0500

    Edited

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,14 @@
 window.addEventListener("load", (event)=> {
     
+    const winnerCheck = [[0,1,2],[3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8] , [2,4,6]];
+
+    let xWinnerVal = 0;
+    let oWinnerVal = 0;
     let Xplayer = true;
     let Oplayer = false;
-    let squares  = document.querySelectorAll("#board > div");
+    let active;
+    let squares  = document.querySelectorAll("#board > div")
+    ;
     
     squares.forEach((sq)=> {
         sq.classList.add("square");
@@ -10,7 +16,7 @@ window.addEventListener("load", (event)=> {
     
     });
     var Track =['', '','','','','','', '','']
-    squares.forEach((sq)=>{
+    squares.forEach((sq, index)=>{
         sq.onclick= (event)=>{
             if(Xplayer && sq.innerHTML==" "){
                 sq.classList.add("X");
@@ -24,21 +30,61 @@ window.addEventListener("load", (event)=> {
                 Xplayer = true;
                 Oplayer = false;
             }
+            active = isWinner();
 
+            if (active == 1){
+               document.querySelector("div#status").classList.add("you-won");
+               document.querySelector("div#status").innerHTML = `Congratulations! ${sq.innerHTML} is the Winner!`;
+
+               squares.forEach((square)=>{
+                   square.onclick = (event) => {
+                       event.preventDefault();
+                   }
+               })
+
+            }
+            
         }
+        
 
         sq.addEventListener('mouseover', function handleMouseOver() {
             sq.classList.add('hover');
           });
           
-          // 👇️ Change text color back on mouseout
+          
           sq.addEventListener('mouseout', function handleMouseOut() {
             sq.classList.remove('hover');
           });
 
-    })
+          let isWinner =()=> {
+
+            for (item = 0 ; winnerCheck.length; item++){
+
+                winnerCheck[item].forEach(element => {
+                    if (squares[element].classList.contains("X")){
+                        xWinnerVal++;
+                    }
+                    else if (squares[element].classList.contains("O")){
+                        oWinnerVal++;
+                    }
+                });
+                if(xWinnerVal == 3 || oWinnerVal ==3 ){
+                    return 1;
+                }
+                xWinnerVal = 0;
+                oWinnerVal = 0;
+               if (item < winnerCheck.length - 1){
+                   continue;
+               }
+               return 0;
+
+            }
+         }
+         
+         });
+
     
-    
+   
 
 });
 
